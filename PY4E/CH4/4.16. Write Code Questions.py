d842d156ef3788df89cbef2a6bec0331a3223d9f
sliucll <72225290+sliucll@users.noreply.github.com>
2020-12-10 21:35:26 +0100

    Update 4.16. Write Code Questions.py

--- PY4E/CH4/4.16. Write Code Questions.py
+++ PY4E/CH4/4.16. Write Code Questions.py
@@ -75,4 +75,77 @@ def areaOfCircle(r):
     area = math.pi * r * r
     return area
     
-/*10. 
+/*10. Finish the function to return the average of a list of numbers, but drop the lowest value. However, if the list only has one value then return that. 
+For example, get_avg_drop_lowest([90]) returns 90 and get_avg_drop_lowest([90, 10]) also returns 90.*/
+def get_avg_drop_lowest(num_list):
+    total = sum(num_list)
+    lowest = min(num_list)
+    num = len(num_list)
+    if num == 1:
+        return total
+    else:
+        return (total - lowest) / (num - 1)
+
+/*11. Finish the function below to return the middle characters from the passed string. 
+If the string has less than 3 characters then return the passed string. 
+If the string has an odd length then return the middle character. 
+If the string has an even length return the two middle characters. 
+For example, get_middle(‘abc’) returns ‘b’ and get_middle(‘abcd’) returns ‘bc’.
+def get_middle(str):
+    num_chars =len(str)
+    mid = num_chars // 2
+    if num_chars < 3:
+        return str
+    elif num_chars % 2 == 1:
+        return str[mid]
+    else:
+        return str[mid-1:mid+1]
+    
+/*12. You are driving a little too fast, and a police officer stops you. 
+Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. 
+If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. 
+If speed is 81 or more, the result is 2. Unless it is your birthday – on that day, your speed can be 5 higher in all cases.*/
+def caught_speeding(speed, is_birthday):
+    if is_birthday is True:
+        if speed <= 65:
+            return 0
+        elif speed <= 85:
+            return 1
+        else:
+            return 2
+    else:
+        if speed <= 60:
+            return 0
+        elif speed <= 80:
+            return 1
+        else:
+            return 2
+
+/*13. Finish the function below to return ‘too low’ if the guess is less than the passed target, ‘correct’ if they are equal, and 
+‘too high’ if the guess is greater than the passed target. 
+For example, check_guess(5,7) returns ‘too low’, check_guess(7,7) returns ‘correct’, and check_guess(9,7) returns ‘too high’.*/
+def check_guess(guess, target):
+    if guess < target:
+        return 'too low'
+    elif guess == target:
+        return 'correct'
+    else:
+        return 'too high'
+
+/*14. Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, …6=Sat, and 
+a boolean indicating if we are on vacation, return a string of the form “7:00” indicating when the alarm clock should ring. 
+If we are on vacation and it is a weekend (0 - Saturday or 6 - Sunday) it should return “off” and otherwise return “10:00”. 
+If we are not on vacation and it is a weekend it should return “10:00” and otherwise “7:00”*/
+def alarm_clock(day, vacation):
+    if vacation:
+        if day == 0 or day == 6:
+            return 'off'
+        else: 
+            return '10:00'
+    else:
+        if day == 0 or day == 6:
+            return '10:00'
+        else: 
+            return '7:00'
+
+
