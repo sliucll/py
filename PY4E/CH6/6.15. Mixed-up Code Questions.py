afd80b228f457e78f8256bce1c8d35b9cc166a85
sliucll <72225290+sliucll@users.noreply.github.com>
2020-12-14 14:13:38 +0100

    Create 6.15. Mixed-up Code Questions.py

---
+++ PY4E/CH6/6.15. Mixed-up Code Questions.py
@@ -0,0 +1,46 @@
+#Q-1: The following segment should print the statement, "So happy 4 you!".
+emotion = "So happy "
+print(emotion + str(4) + " you!")
+
+#Q-2: The following program segment should print the phrase, "My new book cost $12".
+item = "new book"
+price = "12"
+print("My " + item + " cost $" + price)
+
+#Q-3: The following program segment should print the phrase, "Sam likes to code".
+person = "Sam "
+thing = "likes to code"
+print(person + thing)
+
+#Q-4: The following program segment should print the phrase, "It takes us 2 hours and 45 minutes to get home from camp". 
+numHours = 2
+numMinutes = 45.0
+print("It takes us " + str(numHours) + " hours and " + str(int(numMinutes)) + " minutes to get home from camp")
+
+#Q-5: The following program segment should print the phrase, "Grace loves grapes".
+feeling = "loves"
+print("Grace " + feeling + " grapes")
+
+#Q-6: The following program segment should print the phrase, "My sheepdog looks like a Muppet". 
+animal = "sheepdog"
+print("My " + animal + " looks like a Muppet")
+
+#Q-7: The following program segment should print the phrase, "3 + 300 + 7 = 310".
+num1 = 3
+num2 = 300
+num3 = 7
+print(str(num1) + " + " + str(num2) + " + " + str(num3) + " = " + str(ans))
+
+#Q-8: The following program segment should print the phrase, "I am a CS wizard". 
+var1 = "I am a "
+var2 = "CS wizard"
+print(var1 + var2)
+
+#Q-9: The following program segment should print the phrase, "'Red' is a primary color, and so is 'blue'".\
+col1 = "'Red'"
+col2 = "'blue'"
+print(col1 + " is a primary color, and so is " + col2)
+
+#Q-10: The following program segment should print the phrase, "Petting dogs makes me happier than anything else".
+var1 = "dogs"
+print("Petting " + var1 + " makes me happier than anything else")
