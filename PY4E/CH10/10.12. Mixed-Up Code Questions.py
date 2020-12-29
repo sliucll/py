e4782da6c8ad345f2b552589dc7208b0a8b4d931
sliucll <72225290+sliucll@users.noreply.github.com>
2020-12-29 21:03:15 +0100

    Update 10.12. Mixed-Up Code Questions.py

--- PY4E/CH10/10.12. Mixed-Up Code Questions.py
+++ PY4E/CH10/10.12. Mixed-Up Code Questions.py
@@ -1,1 +1,9 @@
-#
+#Q-1 Construct a block of code that sorts the string 'txt' into a list of tuples (the first element is the length of the word, the second is the word itself). 
+#Sort the list in terms of word length from longest to shortest.
+txt = 'but soft what light in yonder window breaks'
+words = txt.split()
+t = []
+for word in words:
+    t.append((len(word),word))
+t.sort(reverse = True)
+print(t)
