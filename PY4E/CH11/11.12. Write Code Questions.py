34b0efc64777df091b2afcc0d565c94667b4f25f
sliucll <72225290+sliucll@users.noreply.github.com>
2021-01-19 20:30:38 +0100

    Update 11.12. Write Code Questions.py

--- PY4E/CH11/11.12. Write Code Questions.py
+++ PY4E/CH11/11.12. Write Code Questions.py
@@ -1,1 +1,20 @@
 #1 Find sequences with one uppercase letter followed by an underscore followed by one or more lowercase letters.
+import re
+def text_match(text):
+    pattern = '[A-Z]_[a-z]+'
+    if re.search(pattern,  text):
+            return('Found a match!')
+    else:
+            return('Not matched!')
+
+print(text_match("aab_cbbbc"))
+
+#2 Check if a string begins with a word character. If it does, return “Found a match!”, if not return “Not matched!” 
+import re
+def text_match(text):
+    pattern = '^\w'    
+    if re.search(pattern, text):
+        return('Found a match!')
+    else:
+        return('Not matched!')
+print(text_match("The quick brown fox jumps over the lazy dog."))
